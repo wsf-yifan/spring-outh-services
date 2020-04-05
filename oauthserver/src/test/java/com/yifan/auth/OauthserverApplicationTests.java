76a6c984e0ed61ee20b99035a5c5d28a62e1ded1
wyifan <wyifan@vip.qq.com>
2020-04-05 17:30:06 +0800

    add files

---
+++ oauthserver/src/test/java/com/yifan/auth/OauthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifan.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
