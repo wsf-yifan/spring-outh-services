6ba8244e7a28d620292321105399bb0c067ac1c9
wyifan <wyifan@vip.qq.com>
2020-04-05 18:36:22 +0800

    add codes

---
+++ oauthclient/src/main/java/com/yifan/authclient/config/UiWebConfig.java
@@ -0,0 +1,35 @@
+
+package com.yifan.authclient.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class UiWebConfig  implements WebMvcConfigurer { 
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Override
+    public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/")
+            .setViewName("forward:/index");
+        registry.addViewController("/index");
+        registry.addViewController("/securedPage");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+            .addResourceLocations("/resources/");
+    }
+
+
+}
