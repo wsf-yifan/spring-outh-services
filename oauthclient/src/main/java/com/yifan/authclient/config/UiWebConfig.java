1e1ce4658131a090ad928c380e374a10ce45e736
shaofan <wyifan@live.cn>
2020-04-05 23:34:23 +0800

    修改代码使项目可以成功运行，但仍然无法跑通 很无语

--- oauthclient/src/main/java/com/yifan/authclient/config/UiWebConfig.java
+++ oauthclient/src/main/java/com/yifan/authclient/config/UiWebConfig.java
@@ -1,11 +1,15 @@
 
 package com.yifan.authclient.config;
 
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
-public class UiWebConfig  implements WebMvcConfigurer { 
+public class UiWebConfig  implements WebMvcConfigurer {
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
