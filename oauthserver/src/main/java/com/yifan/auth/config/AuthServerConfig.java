35c0fbabc74cf32ea6db18b4a5a44256048233c2
shaofan <wyifan@live.cn>
2020-04-06 22:38:12 +0800

    调通服务器与client： 1. 服务器的redirecturl必须要与client的login一致，否则报错，此时可以看到跳转的过程 2. 需要获取登录用户信息，client端需要配置服务端可以取到登录用户的接口，否则会提示没权限

--- oauthserver/src/main/java/com/yifan/auth/config/AuthServerConfig.java
+++ oauthserver/src/main/java/com/yifan/auth/config/AuthServerConfig.java
@@ -22,16 +22,19 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
             .checkTokenAccess("isAuthenticated()");
     }
 
+    // 使用注意：redirectUris必须与client端一致，并且
     @Override
     public void configure(final ClientDetailsServiceConfigurer clients) 
     throws Exception {
         clients.inMemory()
-            .withClient("SampleClientId")
-            .secret(passwordEncoder.encode("secret"))
+            .withClient("djdemoapi")
+            .secret(passwordEncoder.encode( "123456"))
             .authorizedGrantTypes("authorization_code")
             .scopes("user_info")
             .autoApprove(true)
-            .redirectUris("http://localhost:8082/ui/login","http://localhost:8083/ui2/login","http://localhost:8082/login")
+                // 必须进行redirectUris的配置，否则请求授权码时会报错：error="invalid_request",
+                // error_description="At least one redirect_uri must be registered with the client."
+            .redirectUris("http://localhost:8082/ui/login","http://localhost:8782/ui/login","http://localhost:8083/ui2/login","http://localhost:8082/login")
         // .accessTokenValiditySeconds(3600)
         ; // 1 hour
     }
--- oauthserver/src/main/java/com/yifan/auth/config/SecurityConfig.java
+++ oauthserver/src/main/java/com/yifan/auth/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
         auth.inMemoryAuthentication()
-            .withUser("john")
+            .withUser("wsf")
             .password(passwordEncoder().encode("123"))
             .roles("USER");
     } // @formatter:on
